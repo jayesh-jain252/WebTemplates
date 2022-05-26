ebe593ce8784ae7d996fca208c4d64484ec70418
jay1234321 <jayesh64674@gmail.com>
2022-05-26 09:31:31 +0530

    Added Backend for Signup

--- react-auth/src/components/Signup.js
+++ react-auth/src/components/Signup.js
@@ -1,16 +1,36 @@
-import React, { useRef } from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 const Signup = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Failed to create an account");
+    }
+    setLoading(false);
+  }
   return (
     <>
       <Card>
         <Card.Body>
+          {error && <Alert variant="danger">{error}</Alert>}
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -23,7 +43,7 @@ const Signup = () => {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button type="submit" className="w-100 mt-2">
+            <Button disabled={loading} type="submit" className="w-100 mt-2">
               Sign Up
             </Button>
           </Form>
